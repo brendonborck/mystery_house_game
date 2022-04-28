93b503a29f7cd67f1d7a03fb63cee61825a3f262
Cosseno <brendonborck@gmail.com>
2022-04-28 17:20:36 -0300

    Rooms

--- constants.py
+++ constants.py
@@ -9,8 +9,11 @@ SCREEN = None
 
 ASSETS_DIR = os.path.join(os.getcwd(), 'assets')
 IMAGES_DIR = os.path.join(ASSETS_DIR, 'images')
-START_IMAGE='start_image.jpg'
-ROOM_IMAGE="room_v2.jpeg"
+START_IMAGE = 'start_image.jpg'
+ROOM_IMAGE = "room_v1.jpeg"
+ROOM_IMAGE_2 = "room_v2.jpeg"
+ROOM_IMAGE_3 = "room_v3.jpeg"
+ROOM_IMAGE_4 = "room_v4.jpeg"
 GRAY = (110, 110, 110)
 BLACK=(0, 0, 0)
 WHITE=(255, 255, 255)
