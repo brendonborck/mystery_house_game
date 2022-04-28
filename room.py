6a81936fe676ccfe21c5786ce412aa4b2074bb4a
Cosseno <brendonborck@gmail.com>
2022-04-28 17:45:04 -0300

    Room

--- room.py
+++ room.py
@@ -7,7 +7,7 @@ import os
 from utils import Utils
 from doors import Door1, Door2
 from paintings import Painting1
-from papers import Paper1, Paper2
+from papers import Paper1, Paper2, Paper5
 from wardrobe import Wardrobe1
 from writingdesk import Writing_Desk1, Writing_Desk2
 
@@ -16,8 +16,8 @@ class Room(pygame.sprite.Sprite):
 
     def __init__(self, name, print_time, player_position, level):
         super().__init__()
-        room_image = constants.ROOM_IMAGE
         
+        room_image = constants.ROOM_IMAGE
         if level == 2:
             room_image = constants.ROOM_IMAGE_2
         elif level == 3:
@@ -80,7 +80,7 @@ def create_rooms():
         'exit_door': {'constructor': Door1, 'parameters':
             {'x': exit_door_x, 'y': exit_door_y, 'position_mode': 'bottomleft'}},
         'paper': {'constructor': Paper1, 'parameters':
-            {'x': paper_x, 'y': paper_y, 'position_mode': 'center'}}
+            {'x': paper_x, 'y': paper_y, 'position_mode': 'center', 'type': 1}}
     }
     room = Room(name, print_time, player_pos, 1)
     room.define_interactive_objects(interactive_objects)
@@ -141,7 +141,8 @@ def create_rooms():
             'parameters': {
                 'x': paper_x,
                 'y': paper_y,
-                'position_mode': 'center'
+                'position_mode': 'center',
+                'type': 1
             }
         }
     }
@@ -205,7 +206,8 @@ def create_rooms():
             'parameters': {
                 'x': paper_x,
                 'y': paper_y,
-                'position_mode': 'center'
+                'position_mode': 'center',
+                'type': 2
             }
         }
     }
@@ -269,7 +271,8 @@ def create_rooms():
             'parameters': {
                 'x': paper_x,
                 'y': paper_y,
-                'position_mode': 'center'
+                'position_mode': 'center',
+                'type': 2
             }
         }
     }
@@ -282,31 +285,17 @@ def create_rooms():
     name = 'Fase 5'
     print_time = 1.2
     player_pos = (int(0.012*constants.WIDTH)*10, int(0.088*constants.HEIGHT)*10)
-    wdesk_x = 0.95*constants.WIDTH
-    wdesk_y = 0.50*constants.HEIGHT
-    bed_x = 0.80*constants.WIDTH
-    bed_y = 0.90*constants.HEIGHT
-    clock_x = 0.30*constants.WIDTH
-    clock_y = 0.36*constants.Y_SUPERIOR_WALL
-    painting_x = 0.15*constants.WIDTH
-    painting_y = 0.36*constants.Y_SUPERIOR_WALL
-    exit_door_x = 0.75*constants.WIDTH
-    exit_door_y = constants.Y_SUPERIOR_WALL
     paper_x = 0.25*constants.WIDTH
     paper_y = 0.8*constants.HEIGHT
     interactive_objects = {
-        'writingdesk': {'constructor': Writing_Desk1,'parameters':
-            {'x': wdesk_x, 'y': wdesk_y, 'position_mode': 'center'}},
-        'painting': {'constructor': Painting1,'parameters':
-            {'x': painting_x, 'y': painting_y, 'position_mode': 'center'}},
-        'bed': {'constructor': Bed1,'parameters':
-            {'x': bed_x, 'y': bed_y, 'position_mode': 'center'}},
-        'clock': {'constructor': Clock1,'parameters':
-            {'x': clock_x, 'y': clock_y, 'position_mode': 'center'}},
-        'exit_door': {'constructor': Door1, 'parameters':
-            {'x': exit_door_x, 'y': exit_door_y, 'position_mode': 'bottomleft'}},
-        'paper': {'constructor': Paper1, 'parameters':
-            {'x': paper_x, 'y': paper_y, 'position_mode': 'center'}}
+        'paper': {
+            'constructor': Paper5,
+            'parameters': {
+                'x': paper_x,
+                'y': paper_y,
+                'position_mode': 'center',
+                'type': 2
+            }
     }
     room = Room(name, print_time, player_pos, 5)
     room.define_interactive_objects(interactive_objects)
